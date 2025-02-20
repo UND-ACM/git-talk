4224016a09e0853f1f77ced0aa5f11a97b5174de
Joseph Melancon <joseph@jmelancon.com>
2025-02-20 11:31:53 -0600

    add ci presentation source

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@
 # requires pandoc and pdflatex
 set -e
 pandoc -f markdown -t beamer UND_ACM_GitTalk.md --highlight-style tango -o UND_ACM_GitTalk.pdf
+pandoc -f markdown -t beamer github-ci.md --highlight-style tango -o github-ci.pdf
